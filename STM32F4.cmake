82c5161645c821ef3f53ffc163c8573ce6567725
Philip Schulz <phs@phisch.org>
2020-04-15 17:18:02 +0200

    Configure include path depending on Target CPU.

--- STM32F4.cmake
+++ STM32F4.cmake
@@ -1,3 +1,5 @@
+message(STATUS "Target System: ${ARCH_NAME} @ ${STM32F4_CPU_TYPE}")
+
 set(TARGET_NAME cmsis)
 set(TARGET_SRC
     ${CMAKE_CURRENT_SOURCE_DIR}/mbed-os/targets/TARGET_STM/TARGET_${ARCH_NAME}/device/system_stm32f4xx.c
@@ -9,5 +11,5 @@ target_include_directories(${TARGET_NAME} PUBLIC
     mbed-os/cmsis/TARGET_CORTEX_M
     mbed-os/targets/TARGET_STM
     mbed-os/targets/TARGET_STM/TARGET_STM32F4/device
-    mbed-os/targets/TARGET_STM/TARGET_${ARCH_NAME}/TARGET_STM32F407xE/device
+    mbed-os/targets/TARGET_STM/TARGET_${ARCH_NAME}/TARGET_${STM32F4_CPU_TYPE}/device
 )
