ace5d70bfd10d7bedd598dba2147195329aa9cba
Philip Schulz <phs@phisch.org>
2020-07-14 20:56:59 +0200

    Make build again on Host to re-enable Unit Test.

--- FreeRTOS.cmake
+++ FreeRTOS.cmake
@@ -10,33 +10,38 @@ get_property(ARCH_NAME GLOBAL PROPERTY ARCH_NAME)
 set(FREERTOS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/FreeRTOS)
 set(FREERTOS_INCLUDE_DIRECTORY ${FREERTOS_DIRECTORY}/include)
 
-if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Generic")
-    set(FREERTOS_PORT_DIRECTORY ${FREERTOS_DIRECTORY}/portable/GCC/ARM_CM4F)
-elseif ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
-    set(FREERTOS_PORT_DIRECTORY ${FREERTOS_DIRECTORY}/portable/GCC/IA32_flat)
-endif()
-
 set(TARGET_NAME FreeRTOS)
-set(TARGET_SRC
-    ${FREERTOS_DIRECTORY}/croutine.c
-    ${FREERTOS_DIRECTORY}/event_groups.c
-    ${FREERTOS_DIRECTORY}/list.c
-    ${FREERTOS_DIRECTORY}/queue.c
-    ${FREERTOS_DIRECTORY}/stream_buffer.c
-    ${FREERTOS_DIRECTORY}/tasks.c
-    ${FREERTOS_DIRECTORY}/timers.c
-    ${FREERTOS_DIRECTORY}/portable/MemMang/heap_4.c
-    ${FREERTOS_PORT_DIRECTORY}/port.c
-)
-add_library(${TARGET_NAME} ${TARGET_SRC})
-target_include_directories(${TARGET_NAME} PUBLIC
-    ${CMAKE_CURRENT_SOURCE_DIR}
-    ${FREERTOS_INCLUDE_DIRECTORY}
-    ${FREERTOS_PORT_DIRECTORY}
-)
+set(FREERTOS_PORT_DIRECTORY ${FREERTOS_DIRECTORY}/portable/GCC/ARM_CM4F)
 
-target_compile_options(${TARGET_NAME} PRIVATE
-    -Wno-error=pointer-to-int-cast
-    -Wno-error=int-to-pointer-cast
-    -Wno-error=type-limits
-)
+if(NOT UNITTEST)
+    set(TARGET_SRC
+        ${FREERTOS_DIRECTORY}/croutine.c
+        ${FREERTOS_DIRECTORY}/event_groups.c
+        ${FREERTOS_DIRECTORY}/list.c
+        ${FREERTOS_DIRECTORY}/queue.c
+        ${FREERTOS_DIRECTORY}/stream_buffer.c
+        ${FREERTOS_DIRECTORY}/tasks.c
+        ${FREERTOS_DIRECTORY}/timers.c
+        ${FREERTOS_DIRECTORY}/portable/MemMang/heap_4.c
+        ${FREERTOS_PORT_DIRECTORY}/port.c
+    )
+    add_library(${TARGET_NAME} ${TARGET_SRC})
+    target_include_directories(${TARGET_NAME} PUBLIC
+        ${CMAKE_CURRENT_SOURCE_DIR}
+        ${FREERTOS_INCLUDE_DIRECTORY}
+        ${FREERTOS_PORT_DIRECTORY}
+    )
+
+    target_compile_options(${TARGET_NAME} PRIVATE
+        -Wno-error=pointer-to-int-cast
+        -Wno-error=int-to-pointer-cast
+        -Wno-error=type-limits
+    )
+else()
+    add_library(${TARGET_NAME} INTERFACE)
+    target_include_directories(${TARGET_NAME} INTERFACE
+        ${CMAKE_CURRENT_SOURCE_DIR}
+        ${FREERTOS_INCLUDE_DIRECTORY}
+        ${FREERTOS_PORT_DIRECTORY}
+    )
+endif()
